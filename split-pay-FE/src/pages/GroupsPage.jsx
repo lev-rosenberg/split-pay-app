6d527421f720dd3adaf37a6af78845a27c33fdeb
JoonPark1 <way11142001@gmail.com>
2024-02-17 09:34:34 -0600

    finished rough layout for FE for each page => profile, create group, and groups pages

--- split-pay-FE/src/pages/GroupsPage.jsx
+++ split-pay-FE/src/pages/GroupsPage.jsx
@@ -1,7 +1,20 @@
+import styles from "../module-styles/GroupsPage.module.css"; 
 const GroupsPage = () => {
     return (
-        <div>
-            <p>This is groups page!</p>
+        <div className={styles["groups-container"]}>
+            <h1>Your Groups</h1>
+            <div className={styles["current-group"]}>
+                <h5>Current Group:</h5>
+                <button type="button">Group 1</button>
+                <button type="button">Group 2</button>
+            </div>
+            <div className={styles["all-groups"]}>
+                <h5>All Groups:</h5>
+                <button type="button">Group 1</button>
+                <button type="button">Awesome Group Name</button>
+                <button type="button">Another Group</button>
+                <button type="button">Group 4</button>
+            </div>
         </div>
     ); 
 }
