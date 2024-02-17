0fb0ef8d798fa262680439890e9bd1ea667a449a
JoonPark1 <way11142001@gmail.com>
2024-02-17 10:33:08 -0600

    worked on dialog pop-up for allowing leader to copy created group join link for sharing on HomePage

--- split-pay-FE/src/pages/HomePage.jsx
+++ split-pay-FE/src/pages/HomePage.jsx
@@ -1,9 +1,23 @@
 import styles from "../module-styles/HomePage.module.css"
 import {useState} from "react"; 
+import Dialog from '@mui/material/Dialog';
+import DialogContent from '@mui/material/DialogContent';
+import DialogActions from '@mui/material/DialogActions';
+import IconButton from '@mui/material/IconButton';
+import ContentCopyIcon from '@mui/icons-material/ContentCopy';
 const HomePage = () => {
     const [groupName, setGroupName] = useState(""); 
+    const [isCreated, setIsCreated] = useState(false); 
+    const [groupLink, setGroupLink] = useState('')
     const handleCreateGroup = () => {
-        console.log("handleCreateGroup called"); 
+        setIsCreated(true); 
+        setGroupLink('https://unique-link-to-group-page'); 
+    }
+    const handleCopyLink = (linkURL) => {
+        navigator.clipboard.writeText(linkURL).then(()=> {
+            console.log('success'); 
+        }).catch(err => console.log(err.message)); 
+        window.alert("successfully copied to your clipboard"); 
     }
     return (
         <div className={styles["home-container"]}>
@@ -13,6 +27,16 @@ const HomePage = () => {
                 <input name="group-name" placeholder="Enter your group here!" type="text" onChange={(e) => setGroupName(e.target.value)}></input>
             </div>
             {groupName.length > 0 && <button type="button" onClick={handleCreateGroup}>Create Group</button>}
+            {isCreated && (
+                <Dialog className={styles["dialog"]} open={isCreated} onClose={() => setIsCreated(false)}>
+                    <DialogContent>Share Group Join Link: <span>{groupLink}</span></DialogContent>
+                    <DialogActions>
+                        <IconButton aria-label="copy link" onClick={() => handleCopyLink(groupLink)}>
+                            <ContentCopyIcon /> 
+                        </IconButton>
+                    </DialogActions>
+                </Dialog>
+            )}
         </div>
     ); 
 }
